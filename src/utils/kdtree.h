420db17791f89da0ffe29ed7419e2a13d5d805f7
NTT1906 <nttispay@gmail.com>
2024-08-28 14:00:33 +0700

    [CHANGE]: multiple changes: - add a method to build tree from vector - readCSVFile() now return a vector contains the dataset - insertDataBalance() will insert then rebuild the tree

--- src/utils/kdtree.h
+++ src/utils/kdtree.h
@@ -27,29 +27,91 @@ void deleteTree(KDTree *root) {
 	delete root;
 }
 
+#include <vector>
+
+
+struct DataCompare{
+	int axis;
+	DataCompare(int axis) : axis(axis) {} // NOLINT(*-explicit-constructor)
+	bool operator()(const Data &d1, const Data &d2) const {
+		return (axis == 0 ? d1.latitude < d2.latitude : d1.longitude < d2.longitude);
+	}
+};
+
+KDTree* buildKDTree(vector<Data> &dataset, unsigned long long l = 0, unsigned long long r = 0, int depth = 0) {
+	if (r >= dataset.size() || r <= l || dataset.empty()) {
+		return nullptr;
+	}
+	size_t m = (l + r) / 2;
+
+	sort(dataset.begin() + l, dataset.begin() + r, DataCompare(depth % 2)); // NOLINT(*-narrowing-conversions)
+	return new KDTree{
+		dataset[m],
+		buildKDTree(dataset, 0, m, depth + 1),
+		buildKDTree(dataset, m + 1, r, depth + 1)
+	};
+}
+
 bool insertData(KDTree *&root, Data &data, int depth = 0) {
 	if (root == nullptr) {
 		root = new KDTree{data, nullptr, nullptr};
 		return true;
 	}
 	if (depth % 2 == 0) {
-		if (data.latitude == root->data.latitude) return false;
 		return insertData((data.latitude < root->data.latitude) ? root->left : root->right, data, depth + 1);
 	}
-	if (data.longitude == root->data.longitude) return false;
 	return insertData((data.longitude < root->data.longitude) ? root->left : root->right, data, depth + 1);
+}
+
+void NLR(KDTree *root, vector<Data> &dataset) {
+	if (root == nullptr) return;
+	dataset.push_back(root->data);
+	NLR(root->left, dataset);
+	NLR(root->right, dataset);
+}
+
+// Insert then rebuild
+void insertDataBalance(KDTree *&root, Data &data) {
+	vector<Data> dataset;
+	NLR(root, dataset);
+	dataset.push_back(data);
+	root = buildKDTree(dataset, 0, dataset.size());
 }
 
 void nearestNeighborSearch(double latitude, double longitude);
 
 void rangeQuery(double leftLat, double leftLong, double rightLat, double rightLong);
 
-KDTree* readCSVFile(const string& filePath) {
+//KDTree* readCSVFile(const string& filePath) {
+//	ifstream file(filePath.c_str());
+//	if (!file.is_open()) {
+//		return nullptr;
+//	}
+//	KDTree *tree{nullptr};
+//	string tmp;
+//	getline(file, tmp); // skip header
+//	while (getline(file, tmp)) {
+//		Data data;
+//		size_t p1 = tmp.find(',');
+//		size_t p2 = tmp.find(',', p1 + 1);
+//		data.city = tmp.substr(0, p1);
+//		if (p2 <= p1) continue;
+//		data.latitude = stod(tmp.substr(p1 + 1, p2 - p1 - 1));
+//		p1 = tmp.find(',', p2 + 1);
+//		if (p1 <= p2) continue;
+//		data.longitude = stod(tmp.substr(p2 + 1, p1));
+//		insertData(tree, data);
+//	}
+//	file.close();
+//	return tree;
+//}
+
+vector<Data> readCSVFile(const string& filePath) {
 	ifstream file(filePath.c_str());
+	vector<Data> dataset;
 	if (!file.is_open()) {
-		return nullptr;
+		return dataset;
 	}
-	KDTree *tree{nullptr};
 	string tmp;
 	getline(file, tmp); // skip header
 	while (getline(file, tmp)) {
@@ -62,10 +124,10 @@ KDTree* readCSVFile(const string& filePath) {
 		p1 = tmp.find(',', p2 + 1);
 		if (p1 <= p2) continue;
 		data.longitude = stod(tmp.substr(p2 + 1, p1));
-		insertData(tree, data);
+		dataset.push_back(data);
 	}
 	file.close();
-	return tree;
+	return dataset;
 }
 
 void printKDTree(KDTree *root = nullptr, const string& prefix = "", bool isLeft = false) {
